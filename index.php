55066b2ae3a4bac3043179d34309f4dbd339ba7a
alancesar145 <404erroralexnotfound@gmail.com>
2025-03-29 21:53:03 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<meta charset=utf-8" />
+<title>Prueba 1</title>
+</head>
+<body>
+<?php
+// Mensaje de bienvenida
+$nombre = 'Angelos';
+echo "Hola, bienvenido $nombre.<br>";
+
+// Operación matemática
+$suma = 4 + 2;
+echo "La suma de 4 + 2 es: $suma";
+?>
+</body>
+</html>
